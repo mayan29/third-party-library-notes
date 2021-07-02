e5375dee8684326236ca365f5482eb249cc3cb08
mayan <18511560993@163.com>
2021-07-02 18:56:00 +0800

    Masonry-1.1.0 初始化

---
+++ FMDB-2.7.5/FMDBNote/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  FMDBNote
+//
+//  Created by mayan on 2021/7/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FMDB-2.7.5/FMDBNote/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  FMDBNote
+//
+//  Created by mayan on 2021/7/2.
+//
+
+#import "ViewController.h"
+#import <FMDB.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = UIColor.orangeColor;
+}
+
+
+@end
